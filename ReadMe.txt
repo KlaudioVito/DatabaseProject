3b5cf3c6b3d81e8741b279a4628f17f8c5445283
Klaudio Vito <kvito00@citymail.cuny.edu>
2017-07-26 15:56:26 -0400

    Database Project
    Worked with a Stock Market database.
    
    Used JDBC and 2 external libraries: MySQL Connector and JFreeChart.

---
+++ ReadMe.txt
@@ -0,0 +1,5 @@
+# Database Project
+
+Worked with a Stock Market database.
+
+Used JDBC and 2 external libraries: MySQL Connector and JFreeChart.
---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/**
+ *
+ * @author Klaudio Vito © 2016
+ */
+
+public class Main {
+    
+    public static void main(String[] args) {
+        Connect connection = new Connect();
+        connection.setResizable(false);
+        connection.setVisible(true);
+    }
+}
